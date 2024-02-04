bff8a4b09b6e338bf1fdb73a135069f55dcee1e4
coder-rs <rahul1330@outlook.com>
2024-02-04 14:17:34 +0530

    add useState to update component dynamically on click

--- Components/01-starting-project/src/App.jsx
+++ Components/01-starting-project/src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./Components/Header/Header";
 import CoreConcept from "./Components/CoreConcept";
+import TabButton from "./Components/TabButton";
+import { EXAMPLES } from "./data";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+  function handleSelect(selectedButton){
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -14,9 +21,24 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[1]} />
           <CoreConcept {...CORE_CONCEPTS[2]} />
           <CoreConcept {...CORE_CONCEPTS[3]} />
-         
           </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect = {() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect = {() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect = {() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect = {() => handleSelect("state")}>State</TabButton>
+          </menu>
+        </section>
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
       </main>
     </div>
   );
